d07dea79c37a231ec702f8de4a0ebe26f4692890
Steven Olthoff <solthoff@umich.edu>
2017-01-26 15:50:01 -0500

    Figuring out template. It builds, but cannot figure out why it fails when I switch to the 9key. I am following this video: https://www.youtube.com/watch?v=qY8V69PxP8Q

---
+++ 9key/9key/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  9key
+//
+//  Created by Steven Olthoff on 1/26/17.
+//  Copyright © 2017 FAKS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 9key/keyboard/KeyboardViewController.swift
@@ -0,0 +1,71 @@
+//
+//  KeyboardViewController.swift
+//  9-Key
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+    
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    var keyboardView: UIView!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+
+        // Load 9kb
+        loadKeyboard();
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+ 
+    }
+    
+    func loadKeyboard() {
+        let keyboardNib = UINib(nibName: "Keyboard", bundle: nil)
+        keyboardView = keyboardNib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        view.addSubview(keyboardView)
+        keyboardView.frame = view.frame
+        nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
+    }
+    
+}
